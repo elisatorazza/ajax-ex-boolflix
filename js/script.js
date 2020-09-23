fbc615c7b888db43cf379ae9dcff1bc38c13712c
Elisa <elisa.torazza@gmail.com>
2020-09-23 14:05:55 +0200

    Visualizzazione con stelle vuote

--- js/script.js
+++ js/script.js
@@ -21,18 +21,19 @@ $(document).ready(function (){
     for (var i = 0; i<movies.length; i++) {
       var vote = parseInt(Math.ceil(movies[i].vote_average) / 2); //Trasformo il voto in un numero da 0 a 5 arrotondato per eccesso
       //Ciclo la lunghezza del voto, in modo da creare ad ogni ciclo una stella. Salvo ogni nuova stella in un array
-        var starList = [];
+        var emptyStar = ["<i class='far fa-star'></i>", "<i class='far fa-star'></i>", "<i class='far fa-star'></i>", "<i class='far fa-star'></i>", "<i class='far fa-star'></i>"];
         for (var j = 0; j<vote; j++) {
           var star = "<i class='fas fa-star'></i>";
-          starList.push(star);
+          emptyStar[j] = star;
         }
-        var newStarList = starList.join(' ');
+        console.log(emptyStar);
+        var starList = emptyStar.join(' ');
 
       var context = {
         "title": movies[i].title,
         "originalTitle": movies[i].original_title,
         "language": movies[i].original_language,
-        "vote": newStarList,
+        "vote": starList,
       };
       var html = template(context);
       $(".movies-list").append(html);
