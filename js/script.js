60ed651737f2b223c14eff2dd736fd2b850950a2
Elisa <elisa.torazza@gmail.com>
2020-09-23 12:21:49 +0200

    Inserire stella con jquery

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function (){
       resetString();
       getMovies(searchText);
     }
-  })
+  });
 //funzione per prendere il template, modificarlo, e inserirlo nel testo
   function renderMovie (movies) {
     var source = $("#movie-template").html();
@@ -21,12 +21,16 @@ $(document).ready(function (){
     for (var i = 0; i<movies.length; i++) {
       var vote = parseInt(Math.ceil(movies[i].vote_average) / 2); //Trasformo il voto in un numero da 0 a 5 arrotondato per eccesso
 
+        for (var j = 0; j<vote; j++) {
+          var star = $("<i class='fas fa-star'></i>");
+          console.log(star);
+        }
       console.log(vote);
       var context = {
         "title": movies[i].title,
         "originalTitle": movies[i].original_title,
         "language": movies[i].original_language,
-        //"vote": movies[i].vote_average,
+        "vote": star,
       };
       var html = template(context);
       $(".movies-list").append(html);
