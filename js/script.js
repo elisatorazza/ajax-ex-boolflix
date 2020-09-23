9d90cca9fb52e6043cb49a0bc845c9d7c56bb404
Elisa <elisa.torazza@gmail.com>
2020-09-23 15:02:26 +0200

    Creato array di bandiere

--- js/script.js
+++ js/script.js
@@ -20,14 +20,52 @@ $(document).ready(function (){
 
     for (var i = 0; i<movies.length; i++) {
       var vote = parseInt(Math.ceil(movies[i].vote_average) / 2); //Trasformo il voto in un numero da 0 a 5 arrotondato per eccesso
-      //Ciclo la lunghezza del voto, in modo da creare ad ogni ciclo una stella. Salvo ogni nuova stella in un array
+        //Creo un array di 5 stelle vuote
         var emptyStar = ["<i class='far fa-star'></i>", "<i class='far fa-star'></i>", "<i class='far fa-star'></i>", "<i class='far fa-star'></i>", "<i class='far fa-star'></i>"];
+        //Ciclo la lunghezza del voto, in modo da creare ad ogni ciclo una stella che sovrascrive un elemento dell'array precedente
         for (var j = 0; j<vote; j++) {
           var star = "<i class='fas fa-star'></i>";
           emptyStar[j] = star;
         }
-        console.log(emptyStar);
-        var starList = emptyStar.join(' ');
+        var starList = emptyStar.join(' '); //Tolgo le virgole dagli elementi dell'array
+
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+
+      //Creo un array di bandiere
+      var flagList = [
+        {
+          "name": "de-DE",
+          "img": "../img/de.svg",
+        },
+        {
+          "name": "es-ES",
+          "img": "../img/es.svg",
+        },
+        {
+          "name": "fr-FR",
+          "img": "../img/fr.svg",
+        },
+        {
+          "name": "en-UK",
+          "img": "../img/gb.svg",
+        },
+        {
+          "name": "it-IT",
+          "img": "../img/it.svg",
+        },
+        {
+          "name": "nl-NL",
+          "img": "../img/nl.svg",
+        },
+        {
+          "name": "ru-RU",
+          "img": "../img/ru.svg",
+        },
+        {
+          "name": "en-US",
+          "img": "../img/us.svg",
+        },
+    ];
 
       var context = {
         "title": movies[i].title,
